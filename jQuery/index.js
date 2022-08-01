d701ea8b302dc4edbc2a55202b7743cfdb55de06
meshachvijaya <93201707+meshachvijaya@users.noreply.github.com>
2022-08-01 09:25:54 +0700

    Add files via upload

---
+++ jQuery/index.js
@@ -0,0 +1,44 @@
+// $("h1").click(function() {
+//     $("h1").css("color", "green")
+// });
+
+$("h1").click(function(){
+    $("h1").hide();
+});
+
+
+// $("button").click(function() {
+//     $("h1").css("color", "purple")
+// });
+
+// $("button").click(function() {
+//     $("h1").toggle();
+// });
+
+// $("button").click(function() {
+//     $("h1").fadeOut();
+// });
+
+// $("button").click(function() {
+//     $("h1").fadeIn();
+// });
+
+// $("button").click(function() {
+//     $("h1").fadeToggle();
+// });
+
+// $("button").click(function() {
+//     $("h1").slideUp();
+// });
+
+// $("button").click(function() {
+//     $("h1").slideDown();
+// });
+
+// $("button").click(function() {
+//     $("h1").slideToggle();
+// });
+
+$("button").click(function() {
+    $("h1").animate({ opacity: 0.5 });
+});
